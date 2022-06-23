b19372ad12148355a29d28652a63b65fad66a18f
Afokeoghene <fokejero@gmail.com>
2022-06-23 16:59:46 -0700

    let's go

---
+++ name.js
@@ -0,0 +1,9 @@
+var firstName = 'Afokeoghene';
+var middleName = 'Nelly';
+var lastName = 'Ojero';
+var height = '1.60m'
+var country = 'Nigeria'
+
+name = firstName + '' + middleName + '' + lastName;
+
+alert("name"+"\n"+"height"+"\n"+"country")
